73dec1b412474c652d53e1d561719a0217c117c6
Lenovo <Lenovo@DESKTOP-2NQVD2J>
2019-06-25 11:55:01 +0530

    commit Task

---
+++ src/main/java/com/app/repo/BookRepository.java
@@ -0,0 +1,10 @@
+package com.app.repo;
+
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import com.app.document.Book;
+
+public interface BookRepository extends MongoRepository<Book, String> {
+
+}
